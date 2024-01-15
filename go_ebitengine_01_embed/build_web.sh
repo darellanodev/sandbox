31c2fc1571581c267b6527b25fa7cacf9be18b98
darellanodev <darellano.aliaga@gmail.com>
2024-01-15 20:16:45 +0100

    change names in build_web.sh and ignore all the wasm

--- go_ebitengine_01_embed/build_web.sh
+++ go_ebitengine_01_embed/build_web.sh
@@ -1,2 +1,2 @@
-env GOOS=js GOARCH=wasm go build -o ./build_web/embedtest.wasm github.com/darellanodev/go-test-embed
+env GOOS=js GOARCH=wasm go build -o ./build_web/embed.wasm github.com/darellanodev/sandbox-go-ebitengine-01-embed
 cp $(go env GOROOT)/misc/wasm/wasm_exec.js ./build_web
--- go_ebitengine_02_img_transformations/build_web.sh
+++ go_ebitengine_02_img_transformations/build_web.sh
@@ -1,2 +1,2 @@
-env GOOS=js GOARCH=wasm go build -o ./build_web/embedtest.wasm github.com/darellanodev/go-test-embed
+env GOOS=js GOARCH=wasm go build -o ./build_web/imgtransformations.wasm github.com/darellanodev/sandbox-go-ebitengine-02-img-transformations
 cp $(go env GOROOT)/misc/wasm/wasm_exec.js ./build_web
