00727e93c52c28e2e358b2d4c34b235feb081846
darellanodev <darellano.aliaga@gmail.com>
2024-01-15 20:04:24 +0100

    add rotating image

--- go_ebitengine_02_flip_sprite/imageutils.go
+++ go_ebitengine_02_flip_sprite/imageutils.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"math"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-func drawNormalImage(screen *ebiten.Image, img *ebiten.Image, posX int, posY int) {
+func getImageCenter(img *ebiten.Image) (float64, float64) {
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
+
+	halfW := float64(width / 2)
+	halfH := float64(height / 2)
+
+	return halfW, halfH 
+}
+
+func drawRotateImage(screen *ebiten.Image, img *ebiten.Image, posX float64, posY float64, degrees float64) {
+	
+	halfW, halfH := getImageCenter(img)
+	
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(1, 1)
-	op.GeoM.Translate(float64(posX), float64(posY))
+	op.GeoM.Translate(-halfW, -halfH)
+	op.GeoM.Rotate(degrees * math.Pi / 180.0)
+	op.GeoM.Translate(halfW, halfH)
+
+	op.GeoM.Translate(posX, posY)
 	screen.DrawImage(img, op)
 }
 
-func drawHorizontalFlippedImage(screen *ebiten.Image, img *ebiten.Image, imageWidth int, posX int, posY int) {
+func drawNormalImage(screen *ebiten.Image, img *ebiten.Image, posX float64, posY float64) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(1, 1)
+	op.GeoM.Translate(posX, posY)
+	screen.DrawImage(img, op)
+}
+
+func drawHorizontalFlippedImage(screen *ebiten.Image, img *ebiten.Image, imageWidth float64, posX float64, posY float64) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(-1, 1)
-	op.GeoM.Translate(float64(posX)+float64(imageWidth), float64(posY))
+	op.GeoM.Translate(posX+imageWidth, posY)
 	screen.DrawImage(img, op)
 }
--- go_ebitengine_02_flip_sprite/main.go
+++ go_ebitengine_02_flip_sprite/main.go
@@ -22,14 +22,28 @@ var assets embed.FS
 
 var (
 	imageEmbeddedFile *ebiten.Image
-	imagePosX  int
-	imagePosY  int
-	imageWidth int
+	imagePosX  float64
+	imagePosY  float64
+	imageWidth float64
+	degrees	   float64
 )
 
 type Game struct{}
 
 func (g *Game) Update() error {
+
+	if imagePosX < appWidth {
+		imagePosX++
+	} else {
+		imagePosX = 0
+	}
+
+	if degrees < 360 {
+		degrees++
+	} else {
+		degrees = 0
+	}
+
 	return nil
 }
 
@@ -37,15 +51,11 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	ebitenutil.DebugPrint(screen, strconv.Itoa(imagePosX) + ", " + strconv.Itoa(imagePosY))
+	ebitenutil.DebugPrint(screen, strconv.Itoa(int(imagePosX)) + ", " + strconv.Itoa(int(imagePosY)))
 
-	if (imagePosX < appWidth) {
-		imagePosX++
-	} else {
-		imagePosX = 0
-	}
 	drawNormalImage(screen, imageEmbeddedFile, imagePosX, imagePosY)
 	drawHorizontalFlippedImage(screen, imageEmbeddedFile, imageWidth, imagePosX, imagePosY + offsetY)
+	drawRotateImage(screen, imageEmbeddedFile, imagePosX, imagePosY + offsetY * 2, degrees)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -57,9 +67,10 @@ func main() {
 	// initialization
 	imageEmbeddedFile, _, _ = ebitenutil.NewImageFromFileSystem(assets,"assets/img/fuel.png")
 
-	imagePosX = 0
-	imagePosY = 0
+	imagePosX  = 0
+	imagePosY  = 0
 	imageWidth = 32
+	degrees    = 0
 
 	ebiten.SetWindowSize(appWidth, appHeight) // Note that the values are equals to layout function
 	ebiten.SetWindowTitle("flip image")
